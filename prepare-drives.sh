57d3f17fc0f3ea1c4a0aa702f5df8e75553e8530
88plug <19512127+88plug@users.noreply.github.com>
2023-04-12 00:22:40 -0700

    Update prepare-drives.sh

--- prepare-drives.sh
+++ prepare-drives.sh
@@ -9,8 +9,9 @@ do
     echo "Last chance..."
     sleep 5
     sudo mkfs.ext4 -L chia-"x"${count} -b 4096 $drive
-    sudo tune2fs -m 0 $drive
-    sudo tune2fs -O ^has_journal -i 0 $drive
-    sudo hdparm -W 0 $drive
-    sudo udisksctl power-off -b $drive
+    sudo tune2fs -m 0 $drive #0% reserve
+    sudo hdparm -W 0 $drive #Disable write-cache
+    sudo udisksctl power-off -b $drive #Power off to finalize hdparm commands.
 done
+
+#    sudo tune2fs -O ^has_journal -i 0 $drive #Removed unless drive used for Farming Node.
