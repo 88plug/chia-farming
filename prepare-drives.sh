4da05d1c3cfc1f171e1a7ec554aebbf278e283c0
88plug <19512127+88plug@users.noreply.github.com>
2023-04-12 00:17:31 -0700

    Create prepare-drives.sh

---
+++ prepare-drives.sh
@@ -0,0 +1,16 @@
+#VERY DANGEROUS SCRIPT
+
+count=0
+for drive in /dev/sdc1 /dev/sdd1 /dev/sde1 /dev/sdf1 /dev/sdg1 /dev/sdh1 /dev/sdi1 /dev/sdj1 /dev/sdk1 /dev/sdl1
+do
+    count=$((count+1))
+    echo "Working on $drive | Going NUCLEAR"
+    sleep 10
+    echo "Last chance..."
+    sleep 5
+    sudo mkfs.ext4 -L chia-"x"${count} -b 4096 $drive
+    sudo tune2fs -m 0 $drive
+    sudo tune2fs -O ^has_journal -i 0 $drive
+    sudo hdparm -W 0 $drive
+    sudo udisksctl power-off -b $drive
+done
