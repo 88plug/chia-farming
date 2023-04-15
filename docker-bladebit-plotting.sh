8c1fd5060fc2cc6a97045f999f3e10032e43177d
88plug <19512127+88plug@users.noreply.github.com>
2023-04-14 22:35:33 -0700

    Update docker-bladebit-plotting.sh

--- docker-bladebit-plotting.sh
+++ docker-bladebit-plotting.sh
@@ -1,8 +1,10 @@
+# Fill in your CONTRACT and FARMERKEY to make plots with 512GB of Memory.
+
 docker kill akash-chia ; docker rm akash-chia
 docker run -d -p 80:8080 \
 -e VERSION=1.6.0 \
--e CONTRACT=xch16txqvdlh67m9stvwmx848xzpgesd60swqxll3rrnnafunuluflds03jkt4 \
--e FARMERKEY=847b826e653279b9e54ce66a1c55cbfdb1ddc4118e70038cdfbaa7e5cb0a785087bc3a6f055f01bbbf84a2c6a3be4a97 \
+-e CONTRACT= \
+-e FARMERKEY= \
 -e PLOTTER=bladebit \
 -e BUCKETS=64 \
 -e PLOT_SIZE=32 \
