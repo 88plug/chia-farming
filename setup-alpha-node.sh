787a4e97f98e757bf6609db76d2367e9a07b9e10
88plug <19512127+88plug@users.noreply.github.com>
2023-04-14 22:36:22 -0700

    Delete setup-alpha-node.sh

--- setup-alpha-node.sh
+++
@@ -1,8 +0,0 @@
-#Configures a Debian LXC Container to be a Farmer
-apt-get install -y sudo aria2 gunzip
-wget https://download.chia.net/dev/chia-blockchain-cli_1.7.1rc2-dev26-6a966913-1_amd64.deb
-dpkg -i chia-blockchain-cli_1.7.1rc2-dev26-6a966913-1_amd64.deb
-chia init
-cd /root/.chia/mainnet/db/
-aria2c https://torrents.chia.net/databases/mainnet/blockchain_v2_mainnet.2023-03-04.sqlite.gz.torrent
-gunzip *.gz ; mv /root/.chia/mainnet/db/blockchain_v2_mainnet.2023-03-04.sqlite.gz /root/.chia/mainnet/db/blockchain_v2_mainnet.sqlite.gz
