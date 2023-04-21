c81d009577f77ecbf5a8317871ae8663abfdd128
88plug <19512127+88plug@users.noreply.github.com>
2023-04-21 13:58:20 -0400

    Update prepare-drives.sh

--- prepare-drives.sh
+++ prepare-drives.sh
@@ -5,15 +5,33 @@ for drive in /dev/sdc1 /dev/sdd1 /dev/sde1 /dev/sdf1 /dev/sdg1 /dev/sdh1 /dev/sd
 do
     count=$((count+1))
     echo "Working on $drive | Going NUCLEAR"
-    sleep 10
-    echo "Last chance..."
-    sleep 5
-    sudo mkfs.ext4 -L chia-"x"${count} -T largefile $drive
-    sudo tune2fs -m 0 $drive #0% reserve
-    sudo hdparm -W 0 $drive #Disable write-cache
-    sudo hdparm --readahead-sector 64 $drive
-#Optimize for Performance: Although a harvester doesn't perform many write operations, you can still optimize the drive for performance. One way to do this is to enable read-ahead caching using the "hdparm" command. For example, you can enable read-ahead caching with a block size of 64 sectors using the following command: sudo hdparm --readahead-sector 64 /dev/sdx. You should replace "sdx" with the device name of the drive that you want to optimize.
-    sudo udisksctl power-off -b $drive #Power off to finalize hdparm commands.
-done
+    #Wipe signatures first // without this ZFS drives blocked
+    wipefs --force --all $drive
+    serial=$(smartctl -i $drive | grep -i "Serial Number" | awk -F ': +' '{print $2}')
+    vendor=$(smartctl -i $drive | grep -i "Vendor" | awk '{print $2}')
+    product=$(smartctl -i $drive | grep -i "Product" | awk '{print $2}')
+    echo "----- Formatting : $vendor | $product | $serial -----"
+    mkfs.ext4 -F -L chia-${serial: -6}-"x"${count} -T largefile -O ^has_journal $drive
+    tune2fs -m 0 $drive #0% reserve
+    hdparm -W 0 $drive #Disable write-cache
+    echo "LABEL=chia-${serial: -4}-x${count} /mnt/chia ext4 noatime,nodiratime,nofail,x-systemd.device-timeout=10 0 0" >> fstab-entries
+    mount_point="/mnt/chia-${serial: -4}-x${count}"
+    mkdir -p $mount_point
+    if mount -t ext4 $drive $mount_point; then
+        if touch $mount_point/temp_file; then
+            echo "SUCCESS on $drive"
+            rm -f $mount_point/temp_file
+            umount $drive
+        else
+            echo "Failed to write to $drive"
+            echo "$vendor | $product | $serial" >> failing_drives.log
+            umount $drive
+        fi
+    else
+        echo "$vendor | $product | $serial_2 | $serial" >> failing_drives.log
+        echo "Failed to mount $drive"
+    fi
+    rmdir $mount_point #remove the mount point directory
 
-#    sudo tune2fs -O ^has_journal -i 0 $drive #Removed unless drive used for Farming Node.
+done
+echo "Check after you run the script in Proxmox for drives to present - any that do not have an ext4 partition are FAILED!"
